5ed9fa54d2253102b59b64a6c0f0fc9b97319806
Phlana <joshua.chang422@gmail.com>
2020-04-14 11:13:20 -0600

    start of game loops

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,51 @@
+use std::io;
 use crate::board_mod::Cell;
 use crate::board_mod::board_mod::BoardLike;
 use crate::connect4::C4Board;
 use crate::toototto::TOBoard;
+use std::mem::take;
+
 mod board_mod;
 mod connect4;
 mod toototto;
 
+fn take_turn(player: i32, width: usize) {
+    println!("Which column will you place a piece in? (1-{})", width);
+    let mut column = String::new();
+    io::stdin().read_line(&mut column).expect("failed to read line");
+    column = column.trim().to_string();
+}
+
+fn start_c4() -> bool {
+    let mut active = true;
+    let mut board = C4Board::new(7, 6);
+    while active {
+        print!("{}", board);
+        take_turn(0, board.get_width());
+    }
+    true
+}
+
+fn start_toototto() -> bool {
+    let mut active = true;
+    let mut board = TOBoard::new(6, 4);
+
+    true
+}
+
 fn main() {
-    let mut b = C4Board::new(7, 6);
-    println!("{}", b);
+    let mut finished = false;
+    while !finished {
+        println!("Which game would you like to play? (connect-4 or toot-otto)");
+        let mut game = String::new();
+        io::stdin().read_line(&mut game).expect("failed to read line");
+        game = game.trim().to_string();
 
-    // b.place(Cell::TYellow, 0);
-    // b.place(Cell::ORed, 1);
-    // b.place(Cell::ORed, 2);
-    // // b.place(Cell::TYellow, 3);
-    // b.place(Cell::TYellow, 4);
-    // b.place(Cell::ORed, 5);
-    //
-    // println!("{}", b);
-    // b.place(Cell::TYellow, 3);
-    // println!("{}", b);
-
-
-    b.place(Cell::TYellow, 0);
-    println!("{}", b);
-    b.place(Cell::ORed, 0);
-    println!("{}", b);
-    b.place(Cell::TYellow, 0);
-    println!("{}", b);
-    b.place(Cell::ORed, 0);
-    println!("{}", b);
-    b.place(Cell::TYellow, 1);
-    println!("{}", b);
-    b.place(Cell::TYellow, 1);
-    println!("{}", b);
-    b.place(Cell::TYellow, 2);
-    println!("{}", b);
-    b.place(Cell::TYellow, 2);
-    println!("{}", b);
-    b.place(Cell::TYellow, 2);
-    println!("{}", b);
-    b.place(Cell::ORed, 3);
-    println!("{}", b);
-    b.place(Cell::ORed, 3);
-    println!("{}", b);
-    b.place(Cell::TYellow, 3);
-    println!("{}", b);
-    // b.place(Cell::TYellow, 1);
-    // println!("{}", b);
-    // b.place(Cell::TYellow, 2);
-    // println!("{}", b);
-    b.place(Cell::TYellow, 3);
-    println!("{}", b);
+        match game.as_str() {
+            "connect-4" => { finished = start_c4(); },
+            "toot-otto" => { finished = start_toototto(); },
+            "q" | "-q" | "-quit" | "quit" | "exit" => { finished = true; },
+            _ => { println!("unrecognised input: {}", game); },
+        }
+    }
 }
