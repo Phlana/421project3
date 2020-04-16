9f732646b9e855d177ea749e3e73cfb289890983
Jordan Los <Jordan@Mac-mini.local>
2020-04-16 00:49:11 -0600

    add frontend and build script

--- server/src/app_server/app.rs
+++ server/src/app_server/app.rs
@@ -1,8 +1,11 @@
-use actix_web::{middleware, web, App, HttpServer};
+use actix_web::{
+    error, guard, middleware, web, App, HttpRequest, HttpResponse, HttpServer,
+    Result,
+};
 // use crate::app_server::routes::api;
 use crate::app_server::routes::index;
 use crate::app_server::routes::users;
-use std::io;
+use std::{env, io};
 use r2d2_sqlite::{SqliteConnectionManager};
 use rusqlite::{NO_PARAMS, Connection, Error};
 
@@ -30,6 +33,38 @@ pub fn initialize() -> Result<(),Error>{
         NO_PARAMS,
     )?;
     Ok(())
+}
+
+use actix_files as fs;
+use actix_session::{CookieSession, Session};
+use actix_utils::mpsc;
+use actix_web::http::{header, Method, StatusCode};
+use bytes::Bytes;
+
+
+//#[get("/index")]
+async fn index(_req: HttpRequest) -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../../index.html")) )
+}
+
+use std::path::PathBuf;
+//
+//fn index(_req: HttpRequest) -> Result<fs::NamedFile> {
+//    let path: PathBuf = "./index.html".into();
+//    Ok(fs::NamedFile::open(path)?)
+//}
+//
+//fn wasm(_req: HttpRequest) -> Result<fs::NamedFile> {
+//    let path: PathBuf = "./static/client.wasm".into();
+//    Ok(fs::NamedFile::open(path)?)
+//}
+
+
+async fn wasm(req: HttpRequest) -> Result<fs::NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(fs::NamedFile::open(path)?)
 }
 
 #[actix_rt::main]
@@ -62,6 +97,9 @@ pub async fn run() -> io::Result<()> {
                     .route(web::put().to(index::put_game_id))
                     .route(web::get().to(index::get_game_id))
                     .route(web::delete().to(index::delete_game_id)))
+            .service(fs::Files::new("/", ".").index_file("index.html"))
+//            .route("/app.wasm", web::get().to(wasm))
+//            .service(fs::Files::new("/", ".").show_files_listing())
             // .service(web::resource("/api/posts")
             //         .route(web::get().to(api::get_posts))
             //         .route(web::post().to(api::post_posts)))
